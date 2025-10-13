97a50d641377b1d61d0c459adad8f4bb02de523e
augustetum <auguste.tumaite@gmail.com>
2025-10-13 14:27:42 +0300

    Add transaction implementation

--- Transaction.h
+++ Transaction.h
@@ -10,6 +10,21 @@ private:
     string receiverKey; 
     double amount;        
     time_t timestamp; 
+
+public:
+    string transactionID;
+
+    Transaction(const string& from, const string& to, double amt);
+    
+    string getSender() const;
+    string getReceiver() const;
+    double getAmount() const;
+    time_t getTimestamp() const;
+    string getTransactionId() const;
+    
+    string calculateTransactionId() const;
+
+    void print() const;
 };
 
 #endif
