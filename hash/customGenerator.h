f76c3176ac8ceb87165fbddfa9161c1d153837c7
Astrida Jablonskytė <astrida789@gmail.com>
2025-10-21 14:32:51 +0300

    implemented lidl hash to merkle tree

--- hash/customGenerator.h
+++ hash/customGenerator.h
@@ -4,21 +4,24 @@
 #include <string>
 #include <cstdint>
 
-class HashGenerator {
+class HashGenerator
+{
 private:
     static const int prekesKodai[440];
     static const int LOOKUP_TABLE_SIZE = 440;
     static const int MIXING_ROUNDS = 4;
-    
+
     static const uint64_t MIX_CONSTANT_1 = 0x517CC1B727220A95ULL;
     static const uint64_t MIX_CONSTANT_2 = 0xBF58476D1CE4E5B9ULL;
     static const uint64_t MIX_CONSTANT_3 = 0x94D049BB133111EBULL;
-    
-    inline uint64_t rotl64(uint64_t x, int r) const {
+
+    inline uint64_t rotl64(uint64_t x, int r) const
+    {
         return (x << r) | (x >> (64 - r));
     }
-    
-    inline uint64_t rotr64(uint64_t x, int r) const {
+
+    inline uint64_t rotr64(uint64_t x, int r) const
+    {
         return (x >> r) | (x << (64 - r));
     }
 
